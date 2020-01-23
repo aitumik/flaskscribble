b48eb656367ef5e6d4a97447dc5fc8d81c332612
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-01-24 01:27:25 +0300

    configurations

--- config.py
+++ config.py
@@ -7,4 +7,13 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hardtoguessstring'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     BLOGGING_MAIL_PREFIX = '[Blogging]'
+    BLOGGING_MAIL_SENDER = 'Blogging Admin <blogging@example.com>'
+    BLOGGING_ADMIN = os.environ.get('BLOGGING_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
     
+
