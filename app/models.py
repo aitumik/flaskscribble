f50bfb0a39c5b1a4b66db2a6c0759d80c8b135de
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-01-24 19:27:57 +0300

    verification of password implementation

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -12,8 +13,21 @@ class Role(db.Model):
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(64),unique=True,index=True)
     username = db.Column(db.String(64),unique=True,index=True)
+    password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not readable")
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return "<User %r>" % self.username
