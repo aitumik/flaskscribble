e0e397cd2fe0731f1a8810ae238dc4f196972a4e
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-01-25 05:40:50 +0300

    wrote the login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,8 @@ from flask_login import login_user
 from ..models import User
 from .forms import LoginForm
 
-@auth.route("/",methods=['GET','POST'])
+@auth.route("/login",methods=['GET','POST'])
 def login():
+    form = LoginForm()
     return render_template("auth/login.html",form=form)
 
