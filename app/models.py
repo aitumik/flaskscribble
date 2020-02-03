a6eb7a5630df8dce0a318d94130324ad33851a44
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-02-03 19:54:04 +0300

    Adding the functionality of followers and following

--- app/models.py
+++ app/models.py
@@ -206,5 +206,10 @@ class Post(db.Model):
             db.session.add(p)
             db.session.commit()
 
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key = True)
+    followed_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key = True)
+    timestamp = db.Column(db.DateTime,default = datetime.utcnow)
 
 login_manager.anonymous_user = AnyonymousUser
