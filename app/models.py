72453e6cf7fcff0f397c10733c99e69e44932410
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-04-04 21:00:25 +0300

    created the ability to login via token

--- app/models.py
+++ app/models.py
@@ -159,6 +159,19 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def generate_auth_token(self,expiration):
+        s = Serializer(current_app.config['SECRET_KEY'],expires_in=expiration)
+        return s.dumps({"id":self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
     def can(self, permissions):
         return self.role is not None and (self.role.permissions & permissions) == permissions
 
