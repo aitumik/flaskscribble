3527395647b6eddc39e3d54664016a63e0370f93
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-04-06 12:08:29 +0300

    tested with a local mail server

--- app/email.py
+++ app/email.py
@@ -6,7 +6,7 @@ from threading import Thread
 
 def send_async_mail(app,msg):
     with app.app_context():
-        # mail.send(msg)
+        #mail.send(msg)
         print(msg)
 
 
@@ -14,7 +14,7 @@ def send_email(to,subject,template,**kwargs):
     app = current_app._get_current_object()
     msg = Message(app.config['BLOGGING_MAIL_SUBJECT_PREFIX'] + ' ' + subject,sender=app.config['BLOGGING_MAIL_SENDER'],recipients=[to])
     msg.body = render_template(template + '.txt',**kwargs)
-    msg.html = render_template(template + '.html',**kwargs)
+    #msg.html = render_template(template + '.html',**kwargs)
     thread = Thread(target=send_async_mail,args=[app,msg])
     thread.start()
 
