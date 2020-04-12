aa07d08a100fa6c884bbe0e6fa86a840463ad9af
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-04-12 14:44:21 +0300

    added the flask moments and flask pagedown

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
+from flask_moment import Moment
+from flask_pagedown import PageDown
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from config import config
@@ -11,7 +13,8 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 mail = Mail()
 db = SQLAlchemy()
-
+moment = Moment()
+pagedown = PageDown()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -22,6 +25,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     mail.init_app(app)
     db.init_app(app)
+    moment.init_app(app)
+    pagedown.init_app(app)
 
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
