7f0a30420976610ee932232e0dd0676dcd660c83
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-05-07 16:15:15 +0300

    i dont even know what I did

--- app/auth/views.py
+++ app/auth/views.py
@@ -50,6 +50,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user= User.query.filter_by(email=form.email.data).first()
+        print(form.email.data)
         if user is not None and user.verify_password(form.password.data):
             login_user(user,form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.home'))
