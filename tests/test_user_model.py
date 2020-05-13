5c76ecb302843e2b6e346917b05aa52c006b21ff
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-05-13 21:08:37 +0300

    ensured all the tests pass

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,4 +1,5 @@
 import unittest
+from app import db
 from app.models import User,Role,Permission
 from app.models import AnonymousUser 
 
@@ -16,7 +17,6 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password = 'cat')
         self.assertTrue(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
-        self.ctx = self.app.app_context()
     def test_password_salts_are_random(self):
         u = User(password='cat')
         u2 = User(password='cat')
@@ -24,12 +24,15 @@ class UserModelTestCase(unittest.TestCase):
     
     def test_roles_and_permissions(self):
         Role.insert_roles()
-        u = User(email='john@example.com',password='cat')
+        user = User.query.filter_by(email="john@sitepuller.com").first()
+        if user is not None:
+            db.session.delete(user)
+            db.session.commit()
+        u = User(email='john@sitepuller.com',password='cat')
         self.assertTrue(u.can(Permission.WRITE))
         self.assertFalse(u.can(Permission.MODERATE))
 
     def test_anonymous_user(self):
-        u = AnyonymousUser()
+        u = AnonymousUser()
         self.assertFalse(u.can(Permission.FOLLOW))
-
 
