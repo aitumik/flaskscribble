6ab37f0211c76c7b213d3cd2b91de0ee8ca5fb48
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-05-14 15:44:28 +0300

    I dont even know what I did

--- app/main/views.py
+++ app/main/views.py
@@ -211,11 +211,13 @@ def moderate():
 @main.route("/moderate/enable/<int:id>")
 @login_required
 @permission_required(Permission.MODERATE)
-def moderate_enable(id):
-    comment = Comment.query.get_or_404(id)
-    comment.disabled = False
-    db.session.add(comment)
-    return redirect(url_for('.moderate/disable',page=request.args.get('page', 1, type=int)))
+#def moderate_enable(id):
+def moderator():
+    #jcomment = Comment.query.get_or_404(id)
+    #comment.disabled = False
+    #db.session.add(comment)
+    #return redirect(url_for('.moderate_disable',page=request.args.get('page', 1, type=int)))
+    return "You have disable"
 
 @main.route("/moderate/disable/<int:id>")
 @login_required
@@ -224,7 +226,7 @@ def moderate_disable(id):
     comment = Comment.query.get_or_404(id)
     comment.disabled = True
     db.session.add(comment)
-    return redirect(url_for('.moderate/enable',page=request.args.get('page', 1, type=int)))
+    return redirect(url_for('main.moderator'))
 
 
 @main.route("/admin")
