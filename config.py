cd4f39e69409e0ace85b55e033298e292283dc40
Nathan <nathanpykimutai@gmail.com>
2020-12-01 16:27:13 +0300

    applied the config files to be loaded from the environment variables

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hardtoguessstring'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     BLOGGING_MAIL_PREFIX = '[Blogging]'
-    BLOGGING_MAIL_SENDER ='nathanpykimutai@gmail.com' #os.environ.get('MAIL_USERNAME')
-    BLOGGING_ADMIN =  'nathanpykimutai@gmail.com'
+    BLOGGING_ADMIN_EMAIL = os.environ.get('MAIL_ADMIN')
+    BLOGGING_MAIL_SENDER = os.environ.get('MAIL_SENDER')
+    BLOGGING_ADMIN =  os.environ.get('MAIL_ADMIN')
     BLOGGING_MAIL_SUBJECT_PREFIX = "[Blogging]"
     BLOGGING_POSTS_PER_PAGE = 10
     BLOGGING_COMMENTS_PER_PAGE = 10
@@ -21,12 +22,12 @@ class Config:
 class DevelopmentConfig(Config):
 
     DEBUG = True
-    MAIL_SERVER = 'smtp.google.com'
+    MAIL_SERVER = 'smtp.sendgrid.net'
     MAIL_PORT = 587
     #MAIL_USE_SSL = True
     MAIL_USE_TLS = True
-    MAIL_PASSWORD = '@Helloworld254'
-    MAIL_USERNAME = 'nathanpykimutai@gmail.com'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = "sqlite:////" + os.path.join(base_dir,'data-dev.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     #SQLALCHEMY_ECHO = True
@@ -40,7 +41,7 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    
+
     SQLALCHEMY_DATABASE_URI = "sqlite:////" + os.path.join(base_dir,'data.sqlite')
 
 
@@ -51,4 +52,3 @@ config = {
 'production': ProductionConfig,
 'default': DevelopmentConfig
 }
-
